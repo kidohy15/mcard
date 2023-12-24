9ae92552dab6a839e97b24e73bceb1a96cbbe70d
kidohy15 <kidohy307@gmail.com>
2023-12-25 03:57:43 +0900

    전역 스타일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,20 @@ import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import logo from './logo.svg'
 import './App.css'
-
-const bold = css`
-  font-weight: bold;
-`
-
-const containerStyles = css`
-  background-color: pink;
-  ${bold}
-`
-
-const Button = styled.button`
-  width: 200px;
-  height: 100px;
-  ${bold}
-`
+import Text from './components/shared/Text'
 
 function App() {
   return (
-    <div className="App" css={containerStyles}>
-      <Button>스타일 버튼</Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t2" style={{ display: 'blcok', color: 'blue' }}>
+        t2
+      </Text>
+      <Text typography="t3">t3</Text>
+      <Text typography="t4">t4</Text>
+      <Text>t5</Text>
     </div>
   )
 }
